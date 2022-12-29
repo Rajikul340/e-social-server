de550d588acdb968e4c1b34736f32288cd824cec
FFrajibraj779 <mdrajikuli757@gmail.com>
2022-12-29 18:57:02 +0600

    mediadata adde

--- index.js
+++ index.js
@@ -1,17 +1,93 @@
-const express = require('express');
-const cors = require('cors');
-const app= express();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+const app = express();
 const port = process.env.PORT || 5000;
+require("dotenv").config();
 
-app.use(cors())
-app.use(express())
+//middleware
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res)=>{
-    res.send("server is running")
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.lrjyghr.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-})
+async function eSocial() {
+  try {
+    const mediaCollection = client.db("e-social").collection("media-data");
+    const commentCollection = client.db("e-social").collection("commentdata");
+    const userCollection = client.db("e-social").collection("users");
 
+    app.post("/mediadata", async (req, res) => {
+      const mediaData = req.body;
+      const result = await mediaCollection.insertOne(mediaData);
+      res.send(result);
+    });
+    app.post("/users", async (req, res) => {
+      const users = req.body;
+      const result = await userCollection.insertOne(users);
+      res.send(result);
+    });
+    app.get("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log("amir id ta", id);
+      const filter = { _id: ObjectId(id) };
+      console.log("fitler ta ki ", filter);
+      const result = await userCollection.findOne(filter);
+      console.log("akjon user je ke", result);
+      res.send(result);
+    });
+    app.get("/users", async (req, res) => {
+      const query = {};
+      const result = await userCollection.find(query).toArray();
+      res.send(result);
+    });
 
-app.listen(port , () =>{
-    console.log(`server running on ${port}`);
-})
+    // app.put('/users/:id', async(req, res)=>{
+    //   const id = req.params.id;
+    //   const filter = {_id:ObjectId(id)}
+    //     const updateUser = req.body;
+
+    // })
+    app.post("/comment", async (req, res) => {
+      const commentData = req.body;
+      const result = await commentCollection.insertOne(commentData);
+      res.send(result);
+    });
+
+    app.get("/comment", async (req, res) => {
+      const query = {};
+      const cursor = commentCollection.find(query).sort({ time: -1 });
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/mediadata", async (req, res) => {
+      const query = {};
+      const cursor = mediaCollection.find(query).sort({ time: -1 });
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/mediadata/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await mediaCollection.findOne(query);
+      res.send(result);
+    });
+  } catch {}
+}
+
+eSocial().catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("server is running");
+});
+
+app.listen(port, () => {
+  console.log(`server running on ${port}`);
+});
